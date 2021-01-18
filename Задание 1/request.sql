669ae1643e1a76d24bbdd9290b5a8a1c49c9aa89
ksuny <43503836+ksuny@users.noreply.github.com>
2021-01-18 21:48:29 +0300

    Create request.sql

---
+++ Задание 1/request.sql
@@ -0,0 +1,7 @@
+SELECT dept.dept_id, dept.name, count(emp.emp_id)
+FROM Departments dept
+	LEFT JOIN Employees emp
+		ON emp.dept_id = dept.dept_id
+WHERE emp.end_date IS NULL
+	AND emp.start_date >= sysdate   -- Дополнительное условие, если в таблице есть сотрудники, не начавшие работать
+GROUP BY dept.dept_id, dept.name
