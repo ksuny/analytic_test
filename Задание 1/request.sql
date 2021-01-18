4b4e308c11a22de5be6664879ce4a3f9baf9f5d3
ksuny <43503836+ksuny@users.noreply.github.com>
2021-01-18 21:48:55 +0300

    Update request.sql

--- Задание 1/request.sql
+++ Задание 1/request.sql
@@ -3,5 +3,5 @@ FROM Departments dept
 	LEFT JOIN Employees emp
 		ON emp.dept_id = dept.dept_id
 WHERE emp.end_date IS NULL
-	AND emp.start_date >= sysdate   -- Дополнительное условие, если в таблице есть сотрудники, не начавшие работать
+	AND emp.start_date >= sysdate   -- Дополнительное условие, если в таблице есть сотрудники, не начавшие работать 
 GROUP BY dept.dept_id, dept.name
